2e3721f3a9d1112f948b5ba011b4d11fc3a6423a
Svinin Evgenii <esvinin@inno.tech>
2022-11-19 11:24:07 +0300

    new logic and hometask were added

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите число №1:");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите число №2:");
+        int number2 = new Scanner(System.in).nextInt();
+        double quotient = (double) number1 / number2;
+        System.out.println("Сумма этих чисел равна: " + (number1 + number2));
+        System.out.println("Разность этих чисел равна: " + (number1 - number2));
+        System.out.println("Произведение этих чисел равно: " + (number1 * number2));
+        System.out.println("Частное этих чисел равно: " + quotient);
     }
 }
